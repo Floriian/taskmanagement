e68b15031d6542c603439c4f63a336afa8ea75a1
Floriian <lovasifloriian@outlook.hu>
2023-04-02 11:32:15 +0200

    inject user repo

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserRepository } from '../user/entity/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectRepository(User) private userRepository: UserRepository) {}
+
+  async signUp(dto: any) {
+    return true;
+  }
+
+  async signIn(dto: any) {
+    return true;
+  }
+}
