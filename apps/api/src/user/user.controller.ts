203cc565c0ea1196b78324cc2f9a33dbdb530888
Floriian <lovasifloriian@outlook.hu>
2023-04-05 11:13:14 +0200

    return the current user.

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -11,6 +11,6 @@ export class UserController {
 
   @Get()
   getUser(@GetUser() user: User) {
-    return user;
+    return this.userService.getUser(user);
   }
 }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserRepository } from './entity/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepository: UserRepository) {}
+
+  async getUser(user: User) {
+    const findedUser = await this.userRepository.findOne({
+      where: {
+        email: user.email,
+      },
+      relations: ['team'],
+    });
+
+    delete findedUser.password;
+
+    return findedUser;
+  }
+}
