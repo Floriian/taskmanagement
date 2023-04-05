8055c82670145d779d7aa64632899513c9a2fbb6
Floriian <lovasifloriian@outlook.hu>
2023-04-05 18:57:32 +0200

    remove localhost from url

--- apps/client/src/services/auth.service.ts
+++ apps/client/src/services/auth.service.ts
@@ -3,15 +3,9 @@ import { TAuthResponse, TSignIn, TSignUp } from '../types/auth.type';
 
 export const authService = {
   signIn: async (data: TSignIn) => {
-    return axios.post<TAuthResponse>(
-      'http://localhost:3000/api/auth/sign-in',
-      data,
-    );
+    return axios.post<TAuthResponse>('/api/auth/sign-in', data);
   },
   signUp: async (data: TSignUp) => {
-    return axios.post<TAuthResponse>(
-      'http://localhost:3000/api/auth/sign-up',
-      data,
-    );
+    return axios.post<TAuthResponse>('/api/auth/sign-up', data);
   },
 };
