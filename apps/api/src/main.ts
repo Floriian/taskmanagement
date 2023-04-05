42a447b3acd4c722a975fa5b511b4cec1e1f77ee
Floriian <lovasifloriian@outlook.hu>
2023-04-05 14:07:12 +0200

    use global filter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { EnoentExceptionFilter } from './filters/enoent.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.useGlobalFilters(new EnoentExceptionFilter());
+
   app.setGlobalPrefix('/api');
   await app.listen(3000);
 }
