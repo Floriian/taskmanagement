23d2e0be0d78167312574392223070d1a55ba071
Floriian <lovasifloriian@outlook.hu>
2023-04-12 18:31:58 +0200

    create team zodschema & types add id to TTeam

--- apps/client/src/types/team.type.ts
+++ apps/client/src/types/team.type.ts
@@ -1,6 +1,13 @@
 import { TUser } from './user.type';
+import { z } from 'zod';
+
+export const CreateTeamSchema = z.object({
+  teamName: z.string().min(1),
+});
+export type TCreateTeam = z.infer<typeof CreateTeamSchema>;
 
 export type TTeam = {
+  id?: number;
   teamName: string;
   teamInviteCode: string;
   users: TUser[];
