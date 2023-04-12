91832efae805f78bf805d316191443172e0b1647
Floriian <lovasifloriian@outlook.hu>
2023-04-12 18:32:52 +0200

    basics info of the team

---
+++ apps/client/src/components/Team/TeamStatistics.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+import { TTeam } from '../../types';
+
+type Props = {
+  team: Omit<TTeam, 'users'>;
+};
+
+export function TeamStatistics({ team }: Props) {
+  return (
+    <Card
+      variant="elevation"
+      sx={{
+        minWidth: 178,
+        maxWidth: 250,
+        ml: 2,
+        mr: 2,
+      }}
+    >
+      <CardContent>
+        <Typography textAlign="center" variant="subtitle1">
+          Team statistics
+        </Typography>
+        <Typography>
+          Your team is{' '}
+          <Typography
+            variant="body1"
+            component="span"
+            sx={{
+              fontWeight: 'bold',
+              '&:first-letter': {
+                textTransform: 'revert',
+              },
+            }}
+          >
+            {team.teamName}
+          </Typography>
+          .
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
