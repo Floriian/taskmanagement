14dca804f4df8973e2e34855973094bb43e7b31b
Floriian <lovasifloriian@outlook.hu>
2023-04-15 16:06:36 +0200

    barrel export

--- apps/client/src/types/index.ts
+++ apps/client/src/types/index.ts
@@ -1,5 +1,6 @@
 export * from './auth.type';
 export * from './nest.error.type';
 export * from './router.type';
+export * from './task.type';
 export * from './team.type';
 export * from './user.type';
