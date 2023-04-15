ef5d3a7529d5e7d139376e7c71cb42e09c06c2f6
Floriian <lovasifloriian@outlook.hu>
2023-04-15 16:06:30 +0200

    create task type & schema

---
+++ apps/client/src/types/task.type.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+export const CreateTaskSchema = z.object({
+  taskTitle: z.string().min(1),
+  description: z.string().min(10),
+  deadline: z.date(),
+});
+export type TCreateTask = z.infer<typeof CreateTaskSchema>;
+export type Task = TCreateTask & {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  completed: boolean;
+};
