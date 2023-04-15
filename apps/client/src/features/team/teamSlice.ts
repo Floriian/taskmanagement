dad3a6c58d8966235afb9e9e9988bcd75c1aacbf
Floriian <lovasifloriian@outlook.hu>
2023-04-15 17:17:20 +0200

    add tasks to team slice

--- apps/client/src/features/team/teamSlice.ts
+++ apps/client/src/features/team/teamSlice.ts
@@ -1,11 +1,15 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { TeamFeature } from './team.feature.type';
+import { unknown } from 'zod';
 
 const initialState: TeamFeature = {
   teamInviteCode: '',
   teamName: '',
   id: undefined,
+  tasks: [],
 };
+
+const date = new Date();
 
 export const teamSlice = createSlice({
   name: 'team',
@@ -16,7 +20,18 @@ export const teamSlice = createSlice({
         (state.teamInviteCode = action.payload.teamInviteCode),
         (state.teamName = action.payload.teamName);
     },
+    addTask: (state) => {
+      state.tasks.push({
+        completed: true,
+        description: 'description',
+        id: 1,
+        taskTitle: 'task title',
+        createdAt: date,
+        deadline: date,
+        updatedAt: date,
+      });
+    },
   },
 });
 
-export const { setTeam } = teamSlice.actions;
+export const { setTeam, addTask } = teamSlice.actions;
