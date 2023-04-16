6d6c2f5c874ba4c7834c28662956f76ca4c4bdca
Floriian <lovasifloriian@outlook.hu>
2023-04-16 23:46:35 +0200

    responsivity

--- apps/client/src/components/Layout/Menu.tsx
+++ apps/client/src/components/Layout/Menu.tsx
@@ -6,17 +6,30 @@ import {
   Button,
   Toolbar,
   IconButton,
+  useMediaQuery,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '@emotion/react';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
+import { toggleDrawer } from '../../features/ui/ui.slice';
 
 export default function Menu() {
+  const theme = useTheme();
+
+  // @ts-ignore
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+
+  const ui = useAppSelector((state) => state.ui);
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(toggleDrawer());
+  };
+
   return (
     <AppBar
       position="fixed"
       sx={{
-        marginBottom: '1rem',
-        width: `calc(100% - ${240})`,
         zIndex: (theme) => theme.zIndex.drawer + 1,
       }}
     >
@@ -25,22 +38,30 @@ export default function Menu() {
           sx={{
             display: 'flex',
             justifyContent: 'end',
-            width: '100vw',
+            //Todo: button jumping
+            // width: '100%',
           }}
         >
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Box sx={{ display: 'flex' }}>
-            <Button color="inherit">Profile</Button>
-            <Button color="inherit">Logout</Button>
-          </Box>
+          {!matches ? (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                mr: 2,
+              }}
+              onClick={handleClick}
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : null}
+          {matches ? (
+            <Box sx={{ display: 'flex' }}>
+              <Button color="inherit">Profile</Button>
+              <Button color="inherit">Logout</Button>
+            </Box>
+          ) : null}
         </Box>
       </Toolbar>
     </AppBar>
