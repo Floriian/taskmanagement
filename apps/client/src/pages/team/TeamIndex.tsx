b91f6ea1731200eb19265bcdb5388d42b5c451a3
Floriian <lovasifloriian@outlook.hu>
2023-04-18 00:01:39 +0200

    create basic team index page

--- apps/client/src/pages/team/TeamIndex.tsx
+++ apps/client/src/pages/team/TeamIndex.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { Chat } from '../../components';
-import { Box, Container, Drawer, List, ListItem } from '@mui/material';
-
-const drawerWidth = 240;
+import React, { useEffect, useState } from 'react';
+import { Chat, UserCard } from '../../components';
+import { Box, Divider, Typography, Container } from '@mui/material';
+import { useAppSelector } from '../../hooks/redux-hooks';
+import { TUser } from '../../types';
+import { teamService } from '../../services/team.service';
 
 export default function TeamIndex() {
-  return <>teamindex</>;
+  const [teamUsers, setTeamUsers] = useState<TUser[]>();
+  const team = useAppSelector((state) => state.team);
+
+  // useEffect(() => {
+  //   teamService.getTeamMembers(team.id!).then((res) => setTeamUsers(res.users));
+  // }, []);
+
+  return (
+    <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Box>
+        <Typography component="h1">{team.teamName}</Typography>
+        <Divider />
+        <Typography>Team members</Typography>
+        <Box>
+          {team.users.map((user) => (
+            <UserCard user={user} key={user.username} />
+          ))}
+        </Box>
+      </Box>
+    </Container>
+  );
 }
