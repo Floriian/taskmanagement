e12b7f103b57f943da14a1f233ae9a1b9c53a2fa
Floriian <lovasifloriian@outlook.hu>
2023-04-29 13:01:02 +0200

    client: create interceptors.

--- apps/client/src/services/auth.instance.ts
+++ apps/client/src/services/auth.instance.ts
@@ -1,4 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { TNestError } from '../types';
+import { authService } from './auth.service';
+import { store } from '../app/store/store';
+import { removeToken } from '../app/store/features/auth/auth.slice';
 
 const token = () =>
   localStorage.getItem('access_token')
@@ -9,7 +13,25 @@ export const authInstance = axios.create({
   baseURL: '/api',
 });
 authInstance.interceptors.request.use((conf) => {
-  const _token = localStorage.getItem('access_token');
+  const _token = store.getState().auth.userToken;
   conf.headers['Authorization'] = 'Bearer ' + _token;
   return conf;
 });
+
+authInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error instanceof AxiosError) {
+      if (
+        error.response?.status === 401 &&
+        error.response.data.message === 'Invalid Token!'
+      ) {
+        store.dispatch(removeToken());
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
