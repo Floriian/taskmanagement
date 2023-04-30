b9ec0427b6aa4be96f5cb112f19e43d463f817bb
Floriian <lovasifloriian@outlook.hu>
2023-04-30 23:28:42 +0200

    api: register jwt module for chat

--- apps/api/src/chat/chat.module.ts
+++ apps/api/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  imports: [JwtModule.register({})],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
